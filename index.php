c1e5354bf44947824cccad0c9b8121447ed09a85
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2024-09-30 13:36:14 +0200

    added some body and style to index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP 2024</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/main.css">
+</head>
+
+<body>
+<header>
+    <h1>PHP 2024</h1>
+</header>
 <?php
 echo "Testing index prio.";
+?>
+</body>
+
+</html>
 
 
