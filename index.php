7fc8c7eef5269cfd4c69fbe2c5a7965ecd54f760
Darthporkius <cholochullo@hotmail.com>
2024-10-01 21:54:07 +0200

    adding arrays

--- index.php
+++ index.php
@@ -11,15 +11,34 @@
 <header>
     <h1>PHP 2024</h1>
 </header>
-<?php
-$username = "Bob";
-echo "Username: " . $username . "<br>";
-$varTest = $username;
-echo "VarTest: " .  $varTest . "<br>";
-$username = "Joe";
-echo "Changed username: " . $username . "<br>";
-echo "Check varTest: " . $varTest;
-?>
+<section>
+    <header>
+        <h2>Variables</h2>
+    </header>
+    <?php
+    $username = "Bob";
+    echo "Username: " . $username . "<br>";
+    $varTest = $username;
+    echo "VarTest: " .  $varTest . "<br>";
+    $username = "Joe";
+    echo "Changed username: " . $username . "<br>";
+    echo "Check varTest: " . $varTest . "<br>";
+    $numberOne = 2;
+    $numberTwo = 2;
+    $sumOfNumbers = $numberOne + $numberTwo;
+    echo "Result of sum: " . $sumOfNumbers;
+    ?>
+</section>
+<section>
+    <header>
+        <h2>Arrays</h2>
+    </header>
+    <?php
+    $testArray = array("bob", "bro", "derp", "Earl");
+    echo "$testArray[1]";
+    ?>
+</section>
+
 </body>
 
 </html>
