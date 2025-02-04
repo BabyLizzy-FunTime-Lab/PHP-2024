c1ff9aac7e669c2b7767bec974ffa031b9c3b354
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2025-02-04 15:57:50 +0100

    populating cats table of publications db

--- tester.php
+++ tester.php
@@ -652,8 +652,33 @@ ISBN: $r4
 </form>
 _END;
     }
+    ?>
+</section>
+<section>
+    <h2>Creating a new Table in db</h2>
+    <?php
+    require_once 'db/login.php';
 
+    try {
+        $pdo = new PDO($attr, $user, $pass, $opts);
+    }
+    catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
 
+    // Make a new table.
+//    $query = "CREATE TABLE cats (
+//       id SMALLINT NOT NULL AUTO_INCREMENT,
+//       family VARCHAR(32) NOT NULL,
+//       name VARCHAR(32) NOT NULL,
+//       age TINYINT NOT NULL,
+//       PRIMARY KEY (id)
+//    )";
+
+    $result = $pdo->query($query);
+
+    $query = "INSERT INTO cats VALUES(NULL, 'Lion', 'Leo', 4), (NULL, 'Cougar', 'Growler', 2), (NULL, 'Cheetah', 'Charly', 3)";
+    $result = $pdo->query($query);
     ?>
 </section>
 <footer style="margin-top: 1em; height: 4em; background-color: cadetblue; text-align: center">
