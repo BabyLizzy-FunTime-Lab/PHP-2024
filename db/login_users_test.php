a7d7f12883b49e1f8ac1f326b65d438e408ec246
Darthporkius <cholochullo@hotmail.com>
2025-03-18 19:40:59 +0100

    updating tester and bunny

---
+++ db/login_users_test.php
@@ -0,0 +1,14 @@
+<?php // login.php
+$host = 'localhost';
+$data = 'users_test';
+$user = 'boss';
+$pass = '123';
+$chrs = 'utf8mb4';
+$attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+$opts =
+    [
+        PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES      => false,
+    ];
+?>
--- tester.php
+++ tester.php
@@ -770,6 +770,99 @@ _END;
     }
     ?>
 </section>
+<section>
+    <h2>MySQL en databases oefening 1</h2>
+    <?php
+    require_once 'db/login_users_test.php';
+
+    try {
+        $pdo = new PDO($attr, $user, $pass, $opts);
+    }
+    catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    if (isset($_POST['name']) &&
+        isset($_POST['email']) &&
+        isset($_POST['password']))
+    {
+        $name = get_post($pdo, 'name');
+        $email = get_post($pdo, 'email');
+        $password = password_hash(get_post($pdo, 'password'), PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $statement = $pdo->prepare($query);
+        $statement->execute([
+                'name' => $name,
+                'email' => $email,
+                'password' => $password
+            ]);
+    }
+    echo <<<_END
+    <form action="tester.php" method="post"><pre>
+         Name <input type="text" name="name">
+        email <input type="text" name="email">
+     password <input type="text" name="password">
+              <input type="submit" value="Add User">
+    </pre></form>
+    _END;
+    ?>
+    <h3>Login to user</h3>
+    <?php
+    require_once 'db/login_users_test.php';
+
+    try {
+        $pdo = new PDO($attr, $user, $pass, $opts);
+    }
+    catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    if (isset($_POST['name_login']) &&
+        isset($_POST['password_login']))
+    {
+        $name = get_post($pdo, 'name_login');
+        $password = get_post($pdo, 'password_login');
+        echo $password;
+        $query = "SELECT name, password FROM users WHERE name = :name";
+        $statement = $pdo->prepare($query);
+        $statement->execute([
+            'name' => $name
+        ]);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+        print_r($user);
+        if($user && password_verify($password, $user['password'])) {
+            echo "Login worked! Welcome " . $user['name'];
+        } else {
+            echo "Login failed.";
+        }
+    }
+    echo <<<_END
+    <form action="tester.php" method="post"><pre>
+         Name <input type="text" name="name_login">
+     password <input type="text" name="password_login">
+              <input type="submit" value="Login">
+    </pre></form>
+    _END;
+    ?>
+</section>
+<section>
+    <h2>Form Handeling</h2>
+    <?php
+    if(!empty($_POST['testName'])) {
+        $testName = $_POST['testName'];
+    } else {
+        $testName = "(Not entered)";
+    }
+    echo <<<_END
+<p>Your name is: $testName</p>
+<form method="post" action="tester.php">
+    <div>What is your name?</div>
+    <input type="text" name="testName">
+    <input type="submit">
+</form>
+_END;
+
+    ?>
+</section>
 <footer style="margin-top: 1em; height: 4em; background-color: cadetblue; text-align: center">
 <h3>Footer</h3>
 </footer>
